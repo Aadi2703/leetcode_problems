93fd00d9893070c4d4ab064f2c3a674736c1e2d0
Aditya <adityakumar20karma@gmail.com>
2026-09-07 16:20:15 +0530

    Time: 4 ms (55.98%), Space: 43.8 MB (26.66%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
             if(ch == '('){
-               
                 if(level >0){
                 result.append(ch);}
                  level++;
